f8b4ba90a2427230906933481cbeea6ef342f370
Cristofer <cristofer.funes@outlook.com>
2023-07-11 16:50:59 +0200

    Added style for first proposal

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ const changeLanguage = () => {
   fetch(`../locales/${selectedLanguage}.json`)
   .then(response => response.json()).then(data => {
     var languageLabels = data.labels;
-    document.getElementById("first_name_label").innerText = languageLabels.first_name;
-    document.getElementById("last_name_label").innerText = languageLabels.last_name;
-    document.getElementById("email_label").innerText = languageLabels.email;
-    document.getElementById("phone_label").innerText = languageLabels.phone;
-    document.getElementById("submit").value = languageLabels.submit;
+    var languageInstructions = data.instructions;
+    document.getElementsByName("first_name_label").forEach(element => element.innerText = languageLabels.first_name);
+    document.getElementsByName("last_name_label").forEach(element => element.innerText = languageLabels.last_name);
+    document.getElementsByName("email_label").forEach(element => element.innerText = languageLabels.email);
+    document.getElementsByName("phone_label").forEach(element => element.innerText = languageLabels.phone);
+    document.getElementsByName("submit").forEach(element => element.value = languageLabels.submit);
+    document.getElementsByName("instructions_title").forEach(element => element.innerText = languageInstructions.instructions_title);
+    document.getElementsByName("instructions_detail").forEach(element => element.innerText = languageInstructions.instructions_detail);
   })
 }
